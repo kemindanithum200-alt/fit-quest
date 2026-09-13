47b42ece975f361114117d95f7630f7459b727a7
kemindanithum200-alt <kemindanithum200@gmail.com>
2026-09-13 21:20:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="fit-quest-v2";
+const CACHE="fit-quest-v4";
 const FILES=["./","./index.html","./manifest.webmanifest","./service-worker.js","./icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
